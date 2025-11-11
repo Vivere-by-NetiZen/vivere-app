a47e21d9461e1dfe128d94a43c484e5d7646e95a
reinhart-c <reinhart.chr@gmail.com>
2025-11-11 15:04:57 +0700

    Feat: Added FinishOnboardingView

---
+++ vivere/Ipad/Screens/Onboarding/FinishOnboarding/FinishOnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  FinishOnboardingView.swift
+//  vivere
+//
+//  Created by Reinhart on 11/11/25.
+//
+
+import SwiftUI
+
+struct FinishOnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinishOnboardingView()
+}
